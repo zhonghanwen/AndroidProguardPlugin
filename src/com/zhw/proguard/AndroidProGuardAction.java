d6220e7e8a4aa0af534945a2269aa0f94c4d4249
zhonghanwen <zhonghanwen@cmcm.com>
2017-10-31 12:43:49 +0800

    fix install error

--- src/com/zhw/proguard/AndroidProGuardAction.java
+++ src/com/zhw/proguard/AndroidProGuardAction.java
@@ -4,8 +4,6 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.ui.GroupedElementsRenderer;
 
 /**
  * Created by zhonghanwen on 2016/6/7.
